02182885b9c549fb791255de2e221ca432aadca7
ivan <vanek.94@list.ru>
2026-04-01 21:04:17 +0300

    update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -43,7 +43,7 @@ export default function Portfolio() {
 			<div className="container">
 
 				<div className="portfolio__about">
-					<strong className="portfolio__experience">Более 2 лет коммерческого опыта во frontend-разработке.</strong>
+					<strong className="portfolio__experience">Более 3 лет коммерческого опыта во frontend-разработке.</strong>
 					<p className="portfolio__description">Разрабатываю <strong>адаптивные</strong> и <strong>кроссбраузерные интерфейсы</strong>, поддерживаю и дорабатываю существующие проекты, реализую <strong>кастомную JavaScript-логику</strong> по ТЗ и умею быстро разбираться в чужом коде.</p>
 					<p className="portfolio__description">Уделяю внимание <strong>производительности</strong> (Google PageSpeed), <strong>семантике</strong>, валидности и чистоте архитектуры.</p>
 					<p className="portfolio__description">Использую <strong>инструменты AI</strong> для ускорения разработки, генерации кода и прототипирования интерфейсов.</p>
